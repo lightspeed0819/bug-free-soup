05dbda45debf9b8db6c1566c05a2c5a38349dc32
Lightspeed <lightspeed0819@duck.com>
2025-11-04 22:47:26 +0530

    Function assign_teachers() now loads subjects from database instead of the csv file

--- utils/assignteachers.py
+++ utils/assignteachers.py
@@ -27,11 +27,7 @@ def assign_teachers():
             chosen = random.choice(least_loaded)
             chosen["load"] += 1
             # Add the chosen teacher to the list of subject teachers.
-            assignments.append({
-                "class": class_name,
-                "subject": sub,
-                "teacher": chosen["ID"],
-            })
+            sql.execute("UPDATE subject_teachers SET teacher = %s WHERE class = %s AND subject = %s;", [chosen["ID"], class_name, sub])
             _log.debug(f"Assigned {chosen['ID']} to {class_name} for {sub}")     # For debugging purposes.
         # Raise error if no eligible teacher found.
         else:
@@ -45,33 +41,21 @@ def assign_teachers():
         t["load"] = 0
     
     # Get the list of subjects for each class.
-    file = open("data/subjectdata.csv")
-    class_subjects = csv.reader(file)
-
-    # List to store assignments
-    assignments = []
-
-    # Skip header row.
-    next(class_subjects)
+    sql.execute("SELECT * FROM subject_teachers;")
+    class_subjects = sql.fetchall()
 
     # Shit gets real here...
     for class_row in class_subjects:
         # Get the class name and the list of subjects.
-        class_name = class_row[0]
-        subjects = class_row[1:]
+        class_name = class_row["class"]
+        subject = class_row["subject"]
 
-        # For each subject...
-        for subject in subjects:
-            # Ignore the cases where the subject is empty...
-            if not subject:
-                continue
-            else:
-                assign(subject)
-    
+        # Ignore the cases where the subject is empty...
+        if subject:
+            assign(subject)
+
     # Close file and MySQL connection.
-    file.close()
+    sql_conn.commit()
     sql_conn.close()
     
     _log.info("Teacher assignment completed.")
-
-    return assignments
